7bbf93fa86dc81cc9ba07a6a7b3c595f181bdd3b
JuniorDurand <junior_durand@outlook.com>
2024-11-25 15:25:54 -0300

    feature(pip-module): Add conf. files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stain_normalization",
+    version="0.1.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wanghao14/Stain_Normalization",
+    packages=find_packages("src"),           
+    package_dir={"": "src"},                 
+    install_requires=[                       
+        "numpy",
+        "patool",
+        "spams"
+    ],
+    python_requires=">=3.5",
+)
